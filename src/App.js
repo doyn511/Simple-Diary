2efc28049fd29ab3ca40c1380b159130d9f3900e
Doyun Lee <doyn511@ajou.ac.kr>
2023-08-30 18:34:40 +0900

    [feat] API 호출

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
-import { useRef, useState } from "react";
-import LifeCycle from "./LifeCycle";
+import React, { useRef, useState, useEffect } from "react";
+
+//https://jsonplaceholder.typicode.com/comments
 
 function App() {
-  const dataId = useRef(0);
   const [data, setData] = useState([]);
+  const dataId = useRef(0);
+
+  const getData = async () => {
+    const res = await fetch(
+      "https://jsonplaceholder.typicode.com/comments"
+    ).then((res) => res.json());
+
+    const initData = res.slice(0, 20).map((it) => {
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        created_date: new Date().getTime(),
+        id: dataId.current++,
+      };
+    });
+    setData(initData);
+  };
+
+  //App 컴포넌트가 Mount 되자마자 getData() 호출
+  useEffect(() => {
+    getData();
+  }, []);
 
   const onCreate = (author, content, emotion) => {
     const created_date = new Date().getTime();
@@ -37,7 +60,6 @@ function App() {
 
   return (
     <div className="App">
-      <LifeCycle />
       <DiaryEditor onCreate={onCreate} />
       <DiaryList onRemove={onRemove} onEdit={onEdit} diaryList={data} />
     </div>
